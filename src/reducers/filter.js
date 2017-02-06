493923cf0338e39c09a41ffb840dccd9aa4b6633
Stephanie Nemeth <traumverloren@gmail.com>
2017-02-06 14:30:50 +0100

    didn't want a map within the same named key, so left it a string instead of another object

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,14 +1,10 @@
 import Immutable from 'immutable';
 
-// todos: in immutable.js arrays are lists :)
-const defaultState = Immutable.Map({});
 
-const filter = (state = defaultState, action) => {
+const filter = (state = 'ALL', action) => {
   switch(action.type) {
     case 'SET_FILTER':
-      return Immutable.Map({
-        filter: action.filter
-      })
+      return action.filter
     default:
       return state;
   }
