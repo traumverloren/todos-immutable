82b3cd09c5cf7d79a52e8f26bac71f57fdb751e1
Stephanie Nemeth <traumverloren@gmail.com>
2017-02-06 15:52:28 +0100

    filtering of todos works now

--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -12,7 +12,6 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     onClick: () => {
       dispatch(setFilter(ownProps.filter))
-
     }
   }
 }
--- src/components/TodoListContainer.js
+++ src/components/TodoListContainer.js
@@ -2,9 +2,22 @@ import { connect } from 'react-redux';
 import TodoList from './TodoList';
 import { toggleTodo } from '../actions';
 
+const filtered = (todos, filter) => {
+  switch(filter) {
+    case 'ALL':
+      return todos;
+    case 'ACTIVE':
+      return todos.filter(todo => todo.get('completed') === false);
+    case 'DONE':
+      return todos.filter(todo => todo.get('completed') === true);
+    default:
+      return todos;
+  }
+}
+
 const mapStateToProps = (state) => {
   return {
-    todos: state.todos
+    todos: filtered(state.todos, state.filter)
   }
 }
 
