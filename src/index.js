30d12bece2cf09aa0f8fb129e41dfeab0f207665
Stephanie Nemeth <traumverloren@gmail.com>
2017-02-09 11:09:34 +0100

    redux-saga is wired up properly

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers/index';
+import createSagaMiddleware from 'redux-saga';
+import { helloSaga } from './sagas';
 
+const sagaMiddleware = createSagaMiddleware();
 const __REDUX_DEVTOOLS_EXTENSION__ = window.__REDUX_DEVTOOLS_EXTENSION__;
-const store = createStore(reducer, __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  reducer,
+  __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(helloSaga);
 
 // test our store is working and action is being dispatched...
 // store.dispatch({ type: 'ADD_TODO', text: 'Feed Wilbur & Dasher'});
